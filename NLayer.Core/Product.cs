4260c8448f6a2f7a8274dd0742f64f9750326125
mrvgulay <mrvgulay@gmail.com>
2022-04-30 12:07:56 +0300

    initial commit

---
+++ NLayer.Core/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace NLayer.Core
+{
+	//Metod içerisindeki field ve metodların default erişim belirteci private
+	//Class'ların ise internal'dır
+	public class Product : BaseEntity
+	{
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public ProductFeature ProductFeature { get; set; }
+    }
+}
+
